4121e7c6a1854c7ecb7b306a66d07677e87e481c
Luca Bevilacqua <bevilacqua.luca05@gmail.com>
2025-11-14 10:45:54 +0100

    Refactor Controller class to encapsulate file handling and improve I/O operations

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,35 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 /**
  * Application controller. Performs the I/O.
  */
-public class Controller {
+class Controller {
 
+    private File file;
+
+    public Controller() {
+        final String home = System.getProperty("user.home");
+        final String sep = System.getProperty("file.separator");
+        this.file = new File(home + sep + "output.txt");
+    }
+
+    public void setFile(final File file) {
+        this.file = file;
+    }
+
+    public String getPath() {
+        return file.getAbsolutePath();
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void write(final String text) throws IOException {
+        Files.writeString(file.toPath(), text);
+    } 
 }
