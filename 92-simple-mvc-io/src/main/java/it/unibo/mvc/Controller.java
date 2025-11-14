997fbd31687238a6e70e24b41697af3ee83158c2
Luca Bevilacqua <bevilacqua.luca05@gmail.com>
2025-11-14 12:22:56 +0100

    Refactor Controller constructor visibility and handle IOException in write method

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -11,7 +11,7 @@ class Controller {
 
     private File file;
 
-    public Controller() {
+    Controller() {
         final String home = System.getProperty("user.home");
         final String sep = System.getProperty("file.separator");
         this.file = new File(home + sep + "output.txt");
@@ -29,7 +29,11 @@ class Controller {
         return file;
     }
 
-    public void write(final String text) throws IOException {
-        Files.writeString(file.toPath(), text);
+    public void write(final String text) {
+        try {
+            Files.writeString(file.toPath(), text);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
     } 
 }
