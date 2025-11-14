2354fd02023491a1ec7d5b4a3f3b91b01ba744f7
Luca Bevilacqua <bevilacqua.luca05@gmail.com>
2025-11-14 17:57:36 +0100

    Enhance Controller interface by adding method signatures for string management and history retrieval

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,32 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- *
+ * it must model a simple controller responsible of I/O access.
  */
 public interface Controller {
+    /**
+     * A method for setting the next string to print. Null values are not acceptable, and an exception should be produced.
+     * @param string string to save.
+    */
+    public void setNextString(final String string);
 
+    /**
+     * A method for getting the next string to print.
+     * @return the next string.
+     */
+    public String getNextString();
+
+    /**
+     * A method for getting the history of the printed strings (in form of a List of Strings).
+     * @return a listo of strings.
+     */
+    public List<String> getHistory();
+    
+    /**
+     * A method that prints the current string. f the current string is unset, an IllegalStateException should be thrown.
+     * @return the current string.
+     */
+    public String getCurrentString();
 }
