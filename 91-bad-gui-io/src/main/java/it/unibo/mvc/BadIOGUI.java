3ae2d1ba396d475fdd222cf68c224775e5d8b577
Luca Bevilacqua <bevilacqua.luca05@gmail.com>
2025-11-17 00:33:37 +0100

    Refactor action listeners in BadIOGUI to use lambda expressions for improved readability

--- 91-bad-gui-io/src/main/java/it/unibo/mvc/BadIOGUI.java
+++ 91-bad-gui-io/src/main/java/it/unibo/mvc/BadIOGUI.java
@@ -6,8 +6,6 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -54,34 +52,27 @@ public class BadIOGUI {
         /*
          * Handlers
          */
-        write.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent ignored) {
-                /*
-                 * This would be VERY BAD in a real application.
-                 *
-                 * This makes the Event Dispatch Thread (EDT) work on an I/O
-                 * operation. I/O operations may take a long time, during which
-                 * your UI becomes completely unresponsive.
-                 */
-                try (PrintStream ps = new PrintStream(path, StandardCharsets.UTF_8)) {
-                    ps.print(randomGenerator.nextInt());
-                } catch (final IOException e) {
-                    JOptionPane.showMessageDialog(frame, e, "Error", JOptionPane.ERROR_MESSAGE);
-                    e.printStackTrace(); // NOPMD: allowed as this is just an exercise
-                }
+        write.addActionListener(e -> {
+            /*
+             * This would be VERY BAD in a real application.
+             *
+             * This makes the Event Dispatch Thread (EDT) work on an I/O
+             * operation. I/O operations may take a long time, during which
+             * your UI becomes completely unresponsive.
+             */
+            try (PrintStream ps = new PrintStream(path, StandardCharsets.UTF_8)) {
+                ps.print(randomGenerator.nextInt());
+            } catch (final IOException err) {
+                JOptionPane.showMessageDialog(frame, err, "Error", JOptionPane.ERROR_MESSAGE);
+                err.printStackTrace(); // NOPMD: allowed as this is just an exercise
             }
         });
-        read.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent event) {
-                try {
-                    final List<String> content = Files.readAllLines(path2);
-                    System.err.println(content.toString()); // NOPMD
-                } catch (final IOException e) {
-                    System.err.println(e); // NOPMD
-
-                }
+        read.addActionListener(e -> {
+            try {
+                final List<String> content = Files.readAllLines(path2);
+                System.err.println(content.toString()); // NOPMD
+            } catch (final IOException err) {
+                System.err.println(err); // NOPMD
 
             }
         });
